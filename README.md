2d2b1d3ba5875c047bd3211a09eb56d7320382da
Ben Cuan <ben@bananiumlabs.com>
2018-10-12 14:33:12 -0700

    Establish project structure, add SP_500_List

---
+++ README.md
@@ -0,0 +1,11 @@
+# AlphaVantage Bot
+
+Collects data on intraday statistics and stores locally.
+
+Developed by [Ben Cuan](https://github.com/dbqeo) and [Alexey Kolechkin](https://github.com/kuxxe)
+
+## Running locally
+
+You need your own AlphaVantage API key to use this bot! Create a file `key.txt` containing only your key in the root directory.
+
+Data is saved in folders named after the date, i.e. `yyyy-mm-dd` within the parent folder `src/data`.
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-let TIME_INTERVAL = 12000; //Time, in milliseconds, between each request.
+/**
+ * Run once a day on or after 13:30 PST for best results.
+ * Fetches intraday data for all SP500 companies and saves them to a folder.
+ * Author: Ben Cuan
+ * Contributions: Alexey Kolechkin
+ */
+
+ import * as fs from 'fs';
+ import a from 'alphavantage';
+
+ const TIME_INTERVAL = 12000; //Time, in milliseconds, between each request.
+ const alpha = a({key: 'key'});
+
 console.log('Server Initialized');
 
 setInterval(() => {
     console.log('Fetching data for ');
+    alpha.data.intraday(`msft`).then(data => {
+        console.log(data);
+    });
 }, TIME_INTERVAL);
