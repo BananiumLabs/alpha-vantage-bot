1f6bd5e4dcdbd71cecd89a29599e2562777cf198
Ben Cuan <ben@bananiumlabs.com>
2018-10-12 14:49:39 -0700

    Read in SP_500_List

--- src/main.js
+++ src/main.js
@@ -10,12 +10,32 @@
 
  const TIME_INTERVAL = 12000; //Time, in milliseconds, between each request.
  const alpha = a({key: 'key'});
+ let stocks; //Array of strings for tickers
 
+console.log('Reading stock ticker list');
+fs.readFile('./src/SP_500_List.txt', 'utf8', (err, data) => {
+    if(err)
+        throw err;
+
+    stocks = data;
+})
+
+stocks = ['test']
 console.log('Server Initialized');
 
-setInterval(() => {
+let currStock = 0;
+let interval = setInterval(() => {
     console.log('Fetching data for ');
-    alpha.data.intraday(`msft`).then(data => {
-        console.log(data);
-    });
+    // alpha.data.intraday(`msft`).then(data => {
+        //     console.log(data);
+        // });
+    currStock++;
+    console.log(stocks.length);
+    console.log(currStock);
+    // if(currStock >= stocks.length) {
+        clearInterval(interval);
+        console.log('Data collection complete');
+    // }
+    
 }, TIME_INTERVAL);
+
