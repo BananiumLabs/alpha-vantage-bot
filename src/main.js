ea32b688aa8e90ee94bb619a6602b3fbac0a96c9
Ben Cuan <ben@bananiumlabs.com>
2018-10-12 15:20:47 -0700

    Autocreate data directories

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@
 
  import * as fs from 'fs';
  import a from 'alphavantage';
+ import {exec} from 'child_process';
+ import * as readline from 'readline';
 
  const TIME_INTERVAL = 12000; //Time, in milliseconds, between each request.
  const alpha = a({key: 'key'});
@@ -20,22 +22,60 @@ fs.readFile('./src/SP_500_List.txt', 'utf8', (err, data) => {
     stocks = data;
 })
 
-stocks = ['test']
-console.log('Server Initialized');
+mkdir();
 
-let currStock = 0;
-let interval = setInterval(() => {
-    console.log('Fetching data for ');
-    // alpha.data.intraday(`msft`).then(data => {
-        //     console.log(data);
-        // });
-    currStock++;
-    console.log(stocks.length);
-    console.log(currStock);
-    // if(currStock >= stocks.length) {
-        clearInterval(interval);
-        console.log('Data collection complete');
-    // }
+console.log('Creating data directory');
+function mkdir() {
+    let today = new Date();
+    let formattedDate = [today.getFullYear(), today.getMonth(), today.getDay()].join('-');
+    console.log('Today is ' + formattedDate);
+    exec ('mkdir ' + 'src/data/' + formattedDate, (err, stdout, stderr) => {
+        if(stderr)
+            if(stderr.toString().includes('cannot create directory')) {
+                const r1 = readline.createInterface({
+                    input: process.stdin, 
+                    output: process.stdout
+                });
+                r1.question('Directory already exists. Overwrite? (y/N)\n', (answer) => {
+                    if(answer === 'y') {
+                        exec('rm -r ' + 'src/data/' + formattedDate, () => {
+                            exec('mkdir ' + 'src/data/' + formattedDate, () => {
+                                r1.close();
+                                runCollection();
+                            });
+                        });
+                    }
+                    else
+                        console.log('Exiting...');
+                })
+
+            }
+            else throw new Error(stderr);
+        else if (err)
+            throw new Error(err);
+        else
+            runCollection();
+    });
+}
+
+
+function runCollection() {
+    console.log('Server Initialized');
     
-}, TIME_INTERVAL);
+    let currStock = 0;
+    let interval = setInterval(() => {
+        console.log('Fetching data for ');
+        // alpha.data.intraday(`msft`).then(data => {
+            //     console.log(data);
+            // });
+        currStock++;
+        console.log(stocks.length);
+        console.log(currStock);
+        if(currStock >= stocks.length) {
+            clearInterval(interval);
+            console.log('Data collection complete');
+        }
+        
+    }, TIME_INTERVAL);
+}
 
